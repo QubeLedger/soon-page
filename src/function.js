87a9368b3dd0a5338a2d167b170d1768f3784e64
knstntn <konstantin@quadrate.org>
2023-11-09 23:56:14 +0500

    add links

---
+++ src/function.js
@@ -0,0 +1,11 @@
+export function Twitter () {
+    window.open("https://twitter.com/QubeLedger");
+  }
+
+export function GitHub () {
+    window.open("https://github.com/QubeLedger");
+  }
+
+export function Telegram () {
+    window.open("https://t.me/QubeLedger");
+  }
